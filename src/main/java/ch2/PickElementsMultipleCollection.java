42daa0a546a4ea2a0257d5286493c6414474e4a4
lukasz_p <merc.angel@hotmail.com>
2021-09-20 07:52:11 +0200

    4

--- src/main/java/ch2/PickElementsMultipleCollection.java
+++ src/main/java/ch2/PickElementsMultipleCollection.java
@@ -2,15 +2,11 @@ package ch2;
 
 import java.util.function.Predicate;
 
-import static ch2.Folks.editors;
-
 public class PickElementsMultipleCollection {
     public static void main( final String[] args ) {
         {
             final long countFriendsStartN = Folks.friends.stream().filter(name -> name.startsWith( "N" ) ).count();
-            // Yuk..
             final long countEditorsStartN = Folks.editors.stream().filter( name -> name.startsWith( "N" ) ).count();
-            // Yuk.
             final long countComradesStartN = Folks.comrades.stream().filter( name -> name.startsWith( "N" ) ).count();
 
             System.out.println( countFriendsStartN );
@@ -19,7 +15,6 @@ public class PickElementsMultipleCollection {
         }
 
         {
-            // Thats better...
             final Predicate<String> startsWithN = name -> name.startsWith( "N" );
 
             final long countFriendsStartN = Folks.friends.stream().filter( startsWithN ).count();
