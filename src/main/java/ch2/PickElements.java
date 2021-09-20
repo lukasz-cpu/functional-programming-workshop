667f53016cab7f775ad889b8adab20d8805f12ff
lukasz_p <merc.angel@hotmail.com>
2021-09-20 07:33:06 +0200

    zxc

---
+++ src/main/java/ch2/PickElements.java
@@ -0,0 +1,32 @@
+package ch2;
+
+import ch2.Folks;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static ch2.Folks.friends;
+
+public class PickElements {
+    public static void main(final String[] args) {
+        {
+            // self-inflicted wound pattern...
+            final List<String> startsWithN = new ArrayList<>();
+            for (String name : friends) {
+                if (name.startsWith("N")) {
+                    startsWithN.add(name);
+                }
+            }
+            System.out.println(String.format("for x in y...Found %d names", startsWithN.size()));
+        }
+
+
+        List<String> n = friends.stream().filter(name -> name.startsWith("N")).collect(Collectors.toList());
+        System.out.println( String.format( "Stream.filter.collect...Found %d names", n.size() ) );
+
+
+    }
+
+
+}
