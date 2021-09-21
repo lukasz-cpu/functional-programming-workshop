03bcb5d5b5fbd8f551089d47d3a420f3092ba909
lukasz_p <merc.angel@hotmail.com>
2021-09-21 20:08:21 +0200

    4

--- src/main/java/ch4/CalculateNAVTest.java
+++ src/main/java/ch4/CalculateNAVTest.java
@@ -1,8 +1,22 @@
 package ch4;
 
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculateNAVTest {
 
 
-
+    @Test
+    public void computeStockWorth() {
+        // Note how easy it is, with lambdas, to "stub" out the priceFinder
+        final CalculateNAV calculateNAV = new CalculateNAV( ticker -> new BigDecimal( "6.01" ) );
+        BigDecimal expected = new BigDecimal( "6010.00" );
+        // I was unaware of the delta thing.
+        assertEquals( 0, calculateNAV.computeStockWorth( "GOOG", 1000 )
+                .compareTo( expected ), 0.001 );
+    }
 
 }
