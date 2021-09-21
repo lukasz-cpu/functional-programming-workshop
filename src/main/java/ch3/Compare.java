36d541e7b0981556d7bd0dfba0cab2c3e6bb2f6d
lukasz_p <merc.angel@hotmail.com>
2021-09-21 12:14:30 +0200

    1

--- src/main/java/ch3/Compare.java
+++ src/main/java/ch3/Compare.java
@@ -18,10 +18,10 @@ public class Compare {
                 new Person( "Jane", 21 ),
                 new Person( "Greg", 35 ) );
 
-        printPeople( "ascendingByAge", sortORama( people, ( p1, p2 ) -> p1.age - p2.age ) );
+        printPeople( "ascendingByAge", sortORama( people, Comparator.comparingInt(p -> p.age)) );
         printPeople( "ascendingByAgeAlt", sortORama( people, Compare::compareByAge ) );
 
-        Comparator<Person> compareAscending = ( p1, p2 ) -> p1.age - p2.age;
+        Comparator<Person> compareAscending = Comparator.comparingInt(p -> p.age);
         Comparator<Person> compareDescending = compareAscending.reversed();
         Comparator<Person> bobsDescending = ( p1, p2 ) -> -compareAscending.compare( p1, p2 );
 
@@ -29,10 +29,9 @@ public class Compare {
         printPeople( "compareDescending", sortORama( people, compareDescending ) );
         printPeople( "bobsDescending", sortORama( people, bobsDescending ) );
 
-        printPeople( "ascendingByName", sortORama( people, ( p1, p2 ) -> p1.name.compareTo( p2.name ) ) );
+        printPeople( "ascendingByName", sortORama( people, Comparator.comparing(p -> p.name)) );
 
-        // Argh. Too bad we don't get partially applied functions then I could
-        // all System.out.printf this
+
         people.stream()
                 .min( compareAscending )
                 .ifPresent( p -> System.out.printf( "youngest %s\n", p ) );
@@ -46,22 +45,13 @@ public class Compare {
                 .thenComparing( byName ) ) );
     }
 
-    /**
-     * Bob just can't quit his procedural ways!?! Am I functional? Is this a
-     * higher order function? Probably not as it does something rather than
-     * return a function that does something.
-     *
-     * @param list of Person to be sorted.
-     * @param c Comparator function
-     * @return
-     */
+
     static List<Person> sortORama( List<Person> list, Comparator<Person> c ) {
         return list.stream()
                 .sorted( c )
                 .collect( toList() );
     }
 
-    // BobK doesn't think the compareByAge method belongs on Person.
     static int compareByAge( Person p1, Person p2 ) {
         return p1.age - p2.age;
     }
